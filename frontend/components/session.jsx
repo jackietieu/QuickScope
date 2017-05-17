67c88b0b0094015227a7ee19ba00e749a45220db
Jackie Tieu <jackietieu2@gmail.com>
2017-05-16 21:03:37 -0700

    fix css for input box

--- frontend/components/session.jsx
+++ frontend/components/session.jsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import SessionFormContainer from './session_form_container';
 
-export default () => (
-  <div className="splash-background-fill">
-    <section className="session-splash">
-      <div className="splash-text">Discover great businesses that are waiting around the corner!</div>
+export default class Session extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false;
+    }
+  }
 
-      <div className="session">
-        <div className="splash-content">
-          <h1 className="splash-title">
-            QuickScope
-          </h1>
-          <p className="splash-description">
-            Login or signup to quickly scope out local businesses and post reviews!
-          </p>
-        </div>
+  componentDidMount() {
+    this.setState({ show: true })
+  }
 
-        <SessionFormContainer />
+  render() {
+    let show = this.state.show ? 'visible' : '';
+
+    return (
+      <div className="splash-background-fill">
+        <section className="session-splash">
+          <div className={`splash-text ${show}`}>Discover great businesses that are waiting around the corner!</div>
+
+          <div className="session">
+            <div className="splash-content">
+              <h1 className="splash-title">
+                QuickScope
+              </h1>
+              <p className="splash-description">
+                Login or signup to quickly scope out local businesses and post reviews!
+              </p>
+            </div>
+
+            <SessionFormContainer />
+          </div>
+        </section>
       </div>
-    </section>
-  </div>
-);
+    )
+  }
+}
